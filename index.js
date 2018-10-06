83f83c8742dca203f0ab490d60eb4b731d931492
Caleb Lemoine <21261388+circa10a@users.noreply.github.com>
2018-10-06 17:23:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function soapRequest(url, headers, xml) {
         },
       });
     }).catch((error) => {
-      console.log('SOAP FAIL: ' + error);
+      console.log(`SOAP FAIL: ${error}`);
       reject(error.response.data);
     });
   });
