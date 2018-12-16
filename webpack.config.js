3f756e5ae165f47d7a36eb5ba48bd28c4eee8ae1
circa10a <caleblemoine@gmail.comS>
2018-12-16 12:35:10 -0600

    add webpack and dist docs

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'easy-soap-request.js',
+    library: 'soapRequest',
+  },
+  devServer: {
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
